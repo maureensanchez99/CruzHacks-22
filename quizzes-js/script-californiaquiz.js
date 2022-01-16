fb414fc887062e8276002486b567fa959b84d1e5
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2022-01-15 17:30:06 -0800

    updating files

---
+++ quizzes-js/script-californiaquiz.js
@@ -0,0 +1,16 @@
+const beginButton = document.getElementById('begin-button')
+
+beginButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('Started')
+    beginButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+   
+}   
+    
+function selectAnswer() {
+
+}
