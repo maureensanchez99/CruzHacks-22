ea2e45d9a64db9d285259fd51e6870320bce309a
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2022-01-16 08:30:41 -0800

    Update script-californiaquiz.js

--- quizzes-js/script-californiaquiz.js
+++ quizzes-js/script-californiaquiz.js
@@ -1,4 +1,5 @@
 const beginButton = document.getElementById('begin-button')
+const nextButton = document.getElementById('next-button')
 const questionContainerElement = document.getElementById('questions')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -17,14 +18,33 @@ function startQuiz() {
 }
 
 function setNextQuestion() {
+    resetState()
    showQuestion(shuffleQuestions[currentQuestionIndex])
 }   
     
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
 
 }
 
@@ -32,7 +52,7 @@ const questions = [
     {
         question: 'Everyone in Calfornia wants to be in the entertainment industry. ',
         answers: [
-            { text: 'True', correct = false},
+            { text: 'True', correct: false},
             { text: 'False', correct: true}
         ]
     }
