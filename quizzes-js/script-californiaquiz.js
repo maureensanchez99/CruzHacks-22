44f7cf04a5ffcfd344b787abeacdf22dc35f8db7
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2022-01-15 18:59:24 -0800

    Update script-californiaquiz.js

--- quizzes-js/script-californiaquiz.js
+++ quizzes-js/script-californiaquiz.js
@@ -1,16 +1,39 @@
 const beginButton = document.getElementById('begin-button')
+const questionContainerElement = document.getElementById('questions')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffleQuestions, currentQuestionIndex //so variables can be redefined later on
 
 beginButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
     console.log('Started')
     beginButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-   
+   showQuestion(shuffleQuestions[currentQuestionIndex])
 }   
     
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'Everyone in Calfornia wants to be in the entertainment industry. ',
+        answers: [
+            { text: 'True', correct = false},
+            { text: 'False', correct: true}
+        ]
+    }
+]
